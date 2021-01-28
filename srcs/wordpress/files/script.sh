9f317a57a4d0b30034439738b741d01943c45856
soukainahikma <soukainahikma@gmail.com>
2021-01-28 14:47:30 +0100

    ft_services

---
+++ srcs/wordpress/files/script.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+openrc default
+rc-service php-fpm7 start
+rc-service nginx reload
+#/etc/init.d/sshd restart
+# ssh deploy@localhost -p 22
+/bin/sh
+#Locate the following line of text in the warning message. Your values of path and x will be different:
+#Offending RSA key in /path/.ssh/known_hosts:x
+#Use your preferred text editor to open the /path/.ssh/known_hosts file.
+#Delete the entire line of text indicated by number x.
+#Save your changes to the file and exit the text editor.
+#You should now be able to connect to the server using SSH.
